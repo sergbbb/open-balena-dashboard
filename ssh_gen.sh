549e3d105ba5751e731bd522ef9e9bd2717bee17
Serhii Khoroshko <khoroshko@gmail.com>
2022-06-25 20:53:43 +0300

    Fixes

--- ssh_gen.sh
+++ ssh_gen.sh
@@ -3,7 +3,7 @@
 FILE=./api/devices
 
 if [ ! -f $FILE ]; then
-   ssh-keygen -t rsa -b 2048 -C "khoroshko@gmail.com" -N "" -f $FILE
+   ssh-keygen -t rsa -b 2048 -C "example@gmail.com" -N "" -f $FILE
  else
     echo "SSH Key $FILE exist. Do nothing!"
 fi
